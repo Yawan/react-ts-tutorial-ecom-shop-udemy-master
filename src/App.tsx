bf1250aff9a41e14c956bf13e1605d0ceae743ba
Joe.Shieh <b97502027@gmail.com>
2021-12-21 17:18:32 +0800

    add Header Navigation

--- src/App.tsx
+++ src/App.tsx
@@ -5,16 +5,20 @@ import CheckoutPage from "./containers/CheckoutPage"
 import AllProductsPage from "./containers/AllProductsPage"
 import HomePage from "./containers/HomePage"
 import { ROUTE } from "./constants/route"
+import HeaderNavigation from "./components/HeaderNavigation"
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path={ROUTE.CHECKOUT} element={<CheckoutPage />} />
-        <Route path={ROUTE.ALL_PRODUCTS} element={<AllProductsPage />} />
-        <Route path={ROUTE.HOME} element={<HomePage />} />
-        <Route path="*" element={<Navigate to={ROUTE.HOME} />} />
-      </Routes>
+      <div className="app-container">
+        <HeaderNavigation />
+        <Routes>
+          <Route path={ROUTE.CHECKOUT} element={<CheckoutPage />} />
+          <Route path={ROUTE.ALL_PRODUCTS} element={<AllProductsPage />} />
+          <Route path={ROUTE.HOME} element={<HomePage />} />
+          <Route path="*" element={<Navigate to={ROUTE.HOME} />} />
+        </Routes>
+      </div>
     </BrowserRouter>
   )
 }
---
+++ src/components/HeaderNavigation/index.tsx
@@ -0,0 +1,28 @@
+import * as React from "react"
+import { Link } from "react-router-dom"
+import { ROUTE } from "../../constants/route"
+import "./style.scss"
+
+interface IHeaderNavigationProps {}
+
+const HeaderNavigation: React.FunctionComponent<IHeaderNavigationProps> = (
+  props
+) => {
+  return (
+    <div className="header-nav-container">
+      <div className="nav-items-left">
+        <Link className="nav-item shopname" to={ROUTE.HOME}>
+          SHOP-HOME
+        </Link>
+        <Link className="nav-item" to={ROUTE.ALL_PRODUCTS}>
+          All Products
+        </Link>
+      </div>
+      <div className="nav-items-right">
+        <i className="nav-item fa fa-shopping-cart"></i>
+      </div>
+    </div>
+  )
+}
+
+export default HeaderNavigation
