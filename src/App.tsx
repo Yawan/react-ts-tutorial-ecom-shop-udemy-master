b23c400491d1ed07f1453bd5358f709caebbaeed
Joe.Shieh <b97502027@gmail.com>
2021-12-27 21:24:38 +0800

    redux store for product details

--- src/App.tsx
+++ src/App.tsx
@@ -6,20 +6,27 @@ import AllProductsPage from "./containers/AllProductsPage"
 import HomePage from "./containers/HomePage"
 import { ROUTE } from "./constants/route"
 import HeaderNavigation from "./components/HeaderNavigation"
+import { createStore } from "redux"
+import { rootReducer } from "./store/reducers"
+import { Provider } from "react-redux"
+
+const store = createStore(rootReducer)
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="app-container">
-        <HeaderNavigation />
-        <Routes>
-          <Route path={ROUTE.CHECKOUT} element={<CheckoutPage />} />
-          <Route path={ROUTE.ALL_PRODUCTS} element={<AllProductsPage />} />
-          <Route path={ROUTE.HOME} element={<HomePage />} />
-          <Route path="*" element={<Navigate to={ROUTE.HOME} />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <div className="app-container">
+          <HeaderNavigation />
+          <Routes>
+            <Route path={ROUTE.CHECKOUT} element={<CheckoutPage />} />
+            <Route path={ROUTE.ALL_PRODUCTS} element={<AllProductsPage />} />
+            <Route path={ROUTE.HOME} element={<HomePage />} />
+            <Route path="*" element={<Navigate to={ROUTE.HOME} />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </Provider>
   )
 }
 
