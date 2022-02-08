4eaee12d7bf48e99b8d2be63aa96bcb637cb88d7
Joe.Shieh <b97502027@gmail.com>
2022-02-08 22:35:18 +0800

    modify API and routes

--- src/api/ShopAPI.ts
+++ src/api/ShopAPI.ts
@@ -1,9 +1,16 @@
 import axios from "axios"
+import { CustomerInfoFieldList } from "../constants/user"
+import { ProductPurchase } from "../store/reducers/userReducer"
 
 export interface GetProductOptions {
   page?: number
   size?: number
   category?: string[]
+}
+
+export interface Order {
+  cart: ProductPurchase[]
+  user: CustomerInfoFieldList
 }
 class ShopAPI {
   baseUrl = "http://localhost:1234"
@@ -17,11 +24,19 @@ class ShopAPI {
     }`
 
     return axios.get(
-      `${this.baseUrl}/products?${pageQueryParam}${sizeQueryParam}${categoryQueryParam}`
+      `${this.baseUrl}/product/all?${pageQueryParam}${sizeQueryParam}${categoryQueryParam}`
     )
   }
+
   getProductFilter = () => {
-    return axios.get(`${this.baseUrl}/productFilters`)
+    return axios.get(`${this.baseUrl}/product/filters`)
+  }
+
+  postOrder = (order: Order) => {
+    const body = {
+      order,
+    }
+    return axios.post(`${this.baseUrl}/order`, body)
   }
 }
 
